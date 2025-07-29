7bee74710c127e78bd6c8af10354c0efcb309813
Flow-Sistemas <suporte@flowsistemas.com>
2025-07-29 09:22:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from converter import convert_dxf_to_geojson
 import requests
 import uuid
@@ -6,27 +7,31 @@ import os
 
 app = FastAPI()
 
+class ConversionRequest(BaseModel):
+    url: str
+
 @app.get("/")
 def root():
     return {"message": "API de conversão DXF para GeoJSON"}
 
 @app.post("/convert")
-def convert(url: str):
+def convert(data: ConversionRequest):
     try:
         file_id = str(uuid.uuid4())
         temp_path = f"/tmp/{file_id}.dxf"
 
-        # Faz download do arquivo DXF
-        r = requests.get(url)
+        # Baixa o arquivo
+        r = requests.get(data.url)
         if r.status_code != 200:
             raise HTTPException(status_code=400, detail="Erro ao baixar o arquivo DXF.")
+        
         with open(temp_path, "wb") as f:
             f.write(r.content)
 
         # Converte
         geojson_data = convert_dxf_to_geojson(temp_path)
 
-        # Limpa o arquivo temporário
+        # Apaga o arquivo temporário
         os.remove(temp_path)
 
         return geojson_data
