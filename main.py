76537168caa732d8379ebf8b876fa90922e3eceb
Flow-Sistemas <suporte@flowsistemas.com>
2025-07-29 09:09:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, HTTPException
+from converter import convert_dxf_to_geojson
+import requests
+import uuid
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "API de conversão DXF para GeoJSON"}
+
+@app.post("/convert")
+def convert(url: str):
+    try:
+        file_id = str(uuid.uuid4())
+        temp_path = f"/tmp/{file_id}.dxf"
+
+        # Faz download do arquivo DXF
+        r = requests.get(url)
+        if r.status_code != 200:
+            raise HTTPException(status_code=400, detail="Erro ao baixar o arquivo DXF.")
+        with open(temp_path, "wb") as f:
+            f.write(r.content)
+
+        # Converte
+        geojson_data = convert_dxf_to_geojson(temp_path)
+
+        # Limpa o arquivo temporário
+        os.remove(temp_path)
+
+        return geojson_data
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
